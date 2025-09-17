47265ba6bdb9cc8a2b822e30e86725978999e02e
mr.anh <lucnao22@gmail.com>
2025-09-18 06:41:17 +0700

    Cập nhật v11.5

--- config/settings.py
+++ config/settings.py
@@ -297,3 +297,12 @@ LOGGING = {
 # ==============================================================================
 if DEBUG:
     NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+# CẤU HÌNH CLOUDINARY CHO MEDIA FILES
+# ==============================================================================
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
